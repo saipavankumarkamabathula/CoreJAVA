420b608bc3c7bdae37be997fcacb57eefc675ba2
User <User@192.168.0.122>
2022-12-16 12:59:53 +0530

    CRUD Operations

---
+++ JDBC/src/com/tns/jdbccrud/Client.java
@@ -0,0 +1,19 @@
+package com.tns.jdbccrud;
+
+import java.sql.SQLException;
+
+import com.tns.jdbccrud.service.Service;
+
+public class Client {
+
+	public static void main(String[] args) throws SQLException {
+		Service service=new Service();
+        //service.addStudent();
+        service.getStudents();
+       //service.updateStudent();
+       //service.deleteStudnet();
+		//service.storedProcedure();
+        //service.closeSession();
+	}
+
+}
